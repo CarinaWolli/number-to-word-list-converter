ad011af852a6248d86c60e641fe89499686d1468
CarinaWolli <wollencarina@gmail.com>
2022-02-11 18:45:29 +0100

    create basic view  with button that calls endpoint

--- pages/api/converter/index.tsx
+++ pages/api/converter/index.tsx
@@ -3,6 +3,8 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
+  const { number } = req.body
+
   const wordList = ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
 
   res.status(200).send({ wordList: wordList });
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,29 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
 export default function Index() {
+  const [number, setNumber] = useState('');
+
+  const convertNumberToWordList = async (e: React.SyntheticEvent) => {
+    e.preventDefault()
+    await axios.post('/api/converter', { 'number': number })
+  }
+
   return (
     <div>
       <h1 className="text-3xl font-bold underline">
-        Hello world!
+        Coding Challenge
       </h1>
+      <form className="mt-8 space-y-3" onSubmit={convertNumberToWordList}>
+        <div className="-space-y-px rounded-md shadow-sm">
+          <div>
+            <input id="number" type="text" value={number} placeholder="Number" onChange={(e) => setNumber(e.target.value)} autoComplete="number" required className="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" />
+          </div>
+        </div>
+        <button type="submit">
+          Convert
+        </button>
+      </form>
     </div>
   );
 };
