854e63ae3d137c4c77a85a5968490017b6b5da2e
CarinaWolli <wollencarina@gmail.com>
2022-02-11 20:00:06 +0100

    clean code

--- pages/api/converter/index.tsx
+++ pages/api/converter/index.tsx
@@ -1,10 +1,9 @@
-
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { number } = req.body
+  const { number } = req.body;
 
-  const wordList = letterCombinations(number)
+  const wordList = letterCombinations(number);
 
   res.status(200).send({ wordList: wordList });
 }
@@ -13,14 +12,14 @@ function letterCombinations(digits: string): string[] {
   if (!digits.length) return [];
 
   const map: { [key: string]: string[] } = {
-    2: ['a', 'b', 'c'],
-    3: ['d', 'e', 'f'],
-    4: ['g', 'h', 'i'],
-    5: ['j', 'k', 'l'],
-    6: ['m', 'n', 'o'],
-    7: ['p', 'q', 'r', 's'],
-    8: ['t', 'u', 'v'],
-    9: ['w', 'x', 'y', 'z'],
+    2: ["a", "b", "c"],
+    3: ["d", "e", "f"],
+    4: ["g", "h", "i"],
+    5: ["j", "k", "l"],
+    6: ["m", "n", "o"],
+    7: ["p", "q", "r", "s"],
+    8: ["t", "u", "v"],
+    9: ["w", "x", "y", "z"],
   };
 
   const result: string[] = [];
@@ -31,10 +30,10 @@ function letterCombinations(digits: string): string[] {
       return;
     }
 
-    for (let x of map[digits[index]]) {
+    for (const x of map[digits[index]]) {
       permute(string + x, index + 1);
     }
   }
-  permute('', 0);
+  permute("", 0);
   return result;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,36 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import axios from "axios";
+import React, { useState } from "react";
 
 export default function Index() {
-  const [number, setNumber] = useState('');
+  const [number, setNumber] = useState("");
+  const [wordList, setWordList] = useState([]);
 
   const convertNumberToWordList = async (e: React.SyntheticEvent) => {
-    e.preventDefault()
-    await axios.post('/api/converter', { 'number': number })
-  }
+    e.preventDefault();
+    const responseWordList = await axios.post("/api/converter", { number: number });
+    setWordList(responseWordList.data.wordList);
+  };
 
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-        Coding Challenge
-      </h1>
+      <h1 className="text-3xl font-bold underline">Coding Challenge</h1>
       <form className="mt-8 space-y-3" onSubmit={convertNumberToWordList}>
         <div className="-space-y-px rounded-md shadow-sm">
           <div>
-            <input id="number" type="text" value={number} placeholder="Number" onChange={(e) => setNumber(e.target.value)} autoComplete="number" required className="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" />
+            <input
+              id="number"
+              type="text"
+              value={number}
+              placeholder="Number"
+              onChange={(e) => setNumber(e.target.value)}
+              autoComplete="number"
+              required
+              className="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            />
           </div>
         </div>
-        <button type="submit">
-          Convert
-        </button>
+        <button type="submit">Convert</button>
       </form>
     </div>
   );
-};
+}
