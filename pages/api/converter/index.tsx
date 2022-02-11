3bc4bb1afe999b36ef6d8a04d9c70d40b957b0c7
CarinaWolli <wollencarina@gmail.com>
2022-02-11 18:10:54 +0100

    fix typo

--- pages/api/converter/index.tsx
+++ pages/api/converter/index.tsx
@@ -5,5 +5,5 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   const wordList = ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
 
-  res.status(200).send({ wordLIst: wordList });
+  res.status(200).send({ wordList: wordList });
 }
